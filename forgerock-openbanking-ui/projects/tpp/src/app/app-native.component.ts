777102edd1e232b984cc0021f4c5a8812ec1e539
Julien Renaux <1388706+shprink@users.noreply.github.com>
2020-03-13 11:08:38 +0100

    remove deprecated feaature (#124)

--- forgerock-openbanking-ui/projects/tpp/src/app/app-native.component.ts
+++ forgerock-openbanking-ui/projects/tpp/src/app/app-native.component.ts
@@ -3,7 +3,6 @@ import { Platform } from '@angular/cdk/platform';
 import { DOCUMENT } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 
-import { ForgerockConfigService } from '@forgerock/openbanking-ngx-common/services/forgerock-config';
 import { ForgerockSplashscreenService } from '@forgerock/openbanking-ngx-common/services/forgerock-splashscreen';
 import { ForgerockGDPRService } from '@forgerock/openbanking-ngx-common/gdpr';
 import { ForgerockNativeSplashscreenService, ForgerockNativeDeepLinkService } from '@forgerock/openbanking-ngx-common/native';
@@ -27,12 +26,9 @@ import { ForgerockNativeSplashscreenService, ForgerockNativeDeepLinkService } fr
   ]
 })
 export class AppNativeComponent {
-  enableCustomization: string = this.configService.get('enableCustomization');
-
   constructor(
     @Inject(DOCUMENT) private document: any,
     private splashscreenService: ForgerockSplashscreenService,
-    private configService: ForgerockConfigService,
     private platform: Platform,
     private translateService: TranslateService,
     private gdprService: ForgerockGDPRService,
